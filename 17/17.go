0d589ade798980e314b999ba7fb8d0ac2558f198
Simon Da Vall <simon.davall@gmail.com>
2025-02-27 11:06:20 +0000

    Aoc2020 Day17

--- 17/17.go
+++ 17/17.go
@@ -39,143 +39,101 @@ func main() {
 	}
 }
 
+type WXYZ struct {
+	w int
+	x int
+	y int
+	z int
+}
+
 func PartOne(grid [][][]byte) int64 {
-	var tally int64 = 0
-
-	cycles := 0
-	for cycles < 6 {
-		cycles++
-		grid = expandGrid(grid)
-
-		var new_grid [][][]byte
-
-		for z := 0; z < len(grid); z++ {
-			var new_plain [][]byte
-			for y := 0; y < len(grid[0]); y++ {
-				var new_row []byte
-				for x := 0; x < len(grid[0][0]); x++ {
-					ch := grid[z][y][x]
-					c := activeNeighbourCount(x, y, z, grid)
-					new_row = updateNewRow(ch, c, new_row)
-				}
-
-				new_plain = append(new_plain, new_row)
-			}
-			new_grid = append(new_grid, new_plain)
-		}
-		grid = new_grid
-	}
-
-	for _, plain := range grid {
-		for _, row := range plain {
-			for _, ch := range row {
+	check := make(map[WXYZ]bool)
+	active := make(map[WXYZ]bool)
+	for z, plain := range grid {
+		for y, row := range plain {
+			for x, ch := range row {
 				if ch == '#' {
-					tally++
+					cur := WXYZ{0, x, y, z}
+					active[cur] = true
+					check = addNeighboursToCheck(cur, check)
 				}
 			}
 		}
 	}
-	return tally
+
+	counter := 0
+	for counter < 6 {
+		counter++
+
+		new_active := make(map[WXYZ]bool)
+		new_check := make(map[WXYZ]bool)
+		for cur := range check {
+			if cur.w != 0 {
+				continue
+			}
+			c := countActiveNeighbours(cur, active)
+			if active[cur] && (c == 2 || c == 3) {
+				new_active[cur] = true
+				new_check = addNeighboursToCheck(cur, new_check)
+			} else if !active[cur] && c == 3 {
+				new_active[cur] = true
+				new_check = addNeighboursToCheck(cur, new_check)
+			}
+		}
+		active = new_active
+		check = new_check
+	}
+	return int64(len(active))
 }
 
-func PartTwo(small_grid [][][]byte) int64 {
-	var hyper_grid [][][][]byte
-	hyper_grid = append(hyper_grid, small_grid)
-
-	cycles := 0
-	for cycles < 6 {
-		cycles++
-		hyper_grid = expandHyperGrid(hyper_grid)
-		var new_hyper [][][][]byte
-
-		for w := 0; w < len(hyper_grid); w++ {
-			var new_grid [][][]byte
-			for z := 0; z < len(hyper_grid[0]); z++ {
-				var new_plain [][]byte
-				for y := 0; y < len(hyper_grid[0][0]); y++ {
-					var new_row []byte
-					for x := 0; x < len(hyper_grid[0][0][0]); x++ {
-						ch := hyper_grid[w][z][y][x]
-						c := activeHyperNeighbourCount(x, y, z, w, hyper_grid)
-						new_row = updateNewRow(ch, c, new_row)
-					}
-
-					new_plain = append(new_plain, new_row)
-				}
-				new_grid = append(new_grid, new_plain)
-			}
-			new_hyper = append(new_hyper, new_grid)
-		}
-		hyper_grid = new_hyper
-	}
-
-	var tally int64 = 0
-	for _, grid := range hyper_grid {
-		for _, plain := range grid {
-			for _, row := range plain {
-				for _, ch := range row {
-					if ch == '#' {
-						tally++
-					}
+func PartTwo(grid [][][]byte) int64 {
+	check := make(map[WXYZ]bool)
+	active := make(map[WXYZ]bool)
+	for z, plain := range grid {
+		for y, row := range plain {
+			for x, ch := range row {
+				if ch == '#' {
+					cur := WXYZ{0, x, y, z}
+					active[cur] = true
+					check = addNeighboursToCheck(cur, check)
 				}
 			}
 		}
 	}
-	return tally
+
+	counter := 0
+	for counter < 6 {
+		counter++
+
+		new_active := make(map[WXYZ]bool)
+		new_check := make(map[WXYZ]bool)
+		for cur := range check {
+			c := countActiveNeighbours(cur, active)
+			if active[cur] && (c == 2 || c == 3) {
+				new_active[cur] = true
+				new_check = addNeighboursToCheck(cur, new_check)
+			} else if !active[cur] && c == 3 {
+				new_active[cur] = true
+				new_check = addNeighboursToCheck(cur, new_check)
+			}
+		}
+		active = new_active
+		check = new_check
+	}
+	return int64(len(active))
 }
 
-func updateNewRow(ch byte, neighbourCount int, new_row []byte) []byte {
-	if ch == '#' {
-		if neighbourCount == 2 || neighbourCount == 3 {
-			new_row = append(new_row, '#')
-		} else {
-			new_row = append(new_row, '.')
-		}
-	}
-	if ch == '.' {
-		if neighbourCount == 3 {
-			new_row = append(new_row, '#')
-		} else {
-			new_row = append(new_row, '.')
-		}
-	}
-	return new_row
-}
-
-func activeNeighbourCount(x int, y int, z int, grid [][][]byte) int {
+func countActiveNeighbours(cur WXYZ, active map[WXYZ]bool) int {
 	count := 0
-	for dx := -1; dx <= 1; dx++ {
-		for dy := -1; dy <= 1; dy++ {
-			for dz := -1; dz <= 1; dz++ {
-				if dx == 0 && dy == 0 && dz == 0 {
-					continue
-				}
-				nx := x + dx
-				ny := y + dy
-				nz := z + dz
-				if !outOfBounds(nx, ny, nz, grid) && grid[nz][ny][nx] == byte('#') {
-					count++
-				}
-			}
-		}
-	}
-	return count
-}
-
-func activeHyperNeighbourCount(x int, y int, z int, w int, hyper [][][][]byte) int {
-	count := 0
-	for dx := -1; dx <= 1; dx++ {
-		for dy := -1; dy <= 1; dy++ {
-			for dz := -1; dz <= 1; dz++ {
-				for dw := -1; dw <= 1; dw++ {
+	for dw := -1; dw <= 1; dw++ {
+		for dx := -1; dx <= 1; dx++ {
+			for dy := -1; dy <= 1; dy++ {
+				for dz := -1; dz <= 1; dz++ {
 					if dx == 0 && dy == 0 && dz == 0 && dw == 0 {
 						continue
 					}
-					nx := x + dx
-					ny := y + dy
-					nz := z + dz
-					nw := w + dw
-					if !outOfBoundsHyper(nx, ny, nz, nw, hyper) && hyper[nw][nz][ny][nx] == byte('#') {
+					neighbour := WXYZ{cur.w + dw, cur.x + dx, cur.y + dy, cur.z + dz}
+					if active[neighbour] {
 						count++
 					}
 				}
@@ -185,17 +143,15 @@ func activeHyperNeighbourCount(x int, y int, z int, w int, hyper [][][][]byte) i
 	return count
 }
 
-func outOfBounds(x int, y int, z int, grid [][][]byte) bool {
-	max_z := len(grid)
-	max_y := len(grid[0])
-	max_x := len(grid[0][0])
-	return x < 0 || x >= max_x || y < 0 || y >= max_y || z < 0 || z >= max_z
-}
-
-func outOfBoundsHyper(x int, y int, z int, w int, hyper [][][][]byte) bool {
-	max_w := len(hyper)
-	max_z := len(hyper[0])
-	max_y := len(hyper[0][0])
-	max_x := len(hyper[0][0][0])
-	return x < 0 || x >= max_x || y < 0 || y >= max_y || z < 0 || z >= max_z || w < 0 || w >= max_w
+func addNeighboursToCheck(cur WXYZ, check map[WXYZ]bool) map[WXYZ]bool {
+	for dw := -1; dw <= 1; dw++ {
+		for dx := -1; dx <= 1; dx++ {
+			for dy := -1; dy <= 1; dy++ {
+				for dz := -1; dz <= 1; dz++ {
+					check[WXYZ{cur.w + dw, cur.x + dx, cur.y + dy, cur.z + dz}] = true
+				}
+			}
+		}
+	}
+	return check
 }
--- 17/readme.md
+++ 17/readme.md
@@ -1,3 +1,7 @@
-# Day 0: The Treachery of Whales #
-https://adventofcode.com/2020/day/00
+# Day 17: Conway Cubes #
+https://adventofcode.com/2020/day/17
 
+
+Day_17 Part 1 result: 240 in 8.47894ms
+
+Day_17 Part 2 result: 1180 in 86.821466ms
